7efdf19aa62d84e88e1cf17bc340c37e015c81ea
Violet <violetchandon@gmail.com>
2022-08-14 06:47:36 -0700

    Setup keep score and displayed winner at game end

--- index.js
+++ index.js
@@ -6,35 +6,37 @@ const drawCards = document.getElementById('draw-cards');
 const cardSlots = document.getElementById('cards');
 const winner = document.getElementById('winner');
 const remaining = document.getElementById('cards-remaining');
+const playerScoreEl = document.getElementById('player-score');
+const computerScoreEl = document.getElementById('computer-score');
+let playerScore = 0;
+let computerScore = 0;
 
-const getNewDeck = () => {
-  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
-    .then((response) => response.json())
-    .then((data) => {
-      deckId = data.deck_id;
-      cardsRemaining(data);
-    });
+const getNewDeck = async () => {
+  const response = await fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/');
+  const data = await response.json();
+  deckId = data.deck_id;
+  cardsRemaining(data);
 };
 
-const getCards = () => {
-  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-    .then((response) => response.json())
-    .then((data) => {
-      cardsRemaining(data);
-      const cardOne = data.cards[0].image;
-      const cardTwo = data.cards[1].image;
+const getCards = async () => {
+  const response = await fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`);
+  const data = await response.json();
+  cardsRemaining(data);
 
-      cardSlots.children[0].innerHTML = `
-        <img class="card" src=${cardOne} />
-      `;
+  const cardOne = data.cards[0].image;
+  const cardTwo = data.cards[1].image;
 
-      cardSlots.children[1].innerHTML = `
-        <img class="card" src=${cardTwo} />      
-      `;
+  cardSlots.children[0].innerHTML = `
+    <img class="card" src=${cardOne} />
+  `;
 
-      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
-      winner.textContent = winnerText;
-    });
+  cardSlots.children[1].innerHTML = `
+    <img class="card" src=${cardTwo} />      
+  `;
+
+  const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+  winner.textContent = winnerText;
+  gameWinner(data);
 };
 
 const determineCardWinner = (card1, card2) => {
@@ -44,8 +46,12 @@ const determineCardWinner = (card1, card2) => {
   const card2ValueIndex = valueOptions.indexOf(card2.value);
 
   if (card1ValueIndex > card2ValueIndex) {
+    computerScore++;
+    computerScoreEl.textContent = `Computer Score: ${computerScore}`;
     return 'Computer Wins!';
   } else if (card1ValueIndex < card2ValueIndex) {
+    playerScore++;
+    playerScoreEl.textContent = `Player Score: ${playerScore}`;
     return 'You win!';
   } else {
     return 'War!';
@@ -57,6 +63,14 @@ const cardsRemaining = (data) => {
     Cards Remaining: ${data.remaining}
     `;
   data.remaining === 0 ? (drawCards.disabled = true) : '';
+};
+
+const gameWinner = (data) => {
+  if (data.remaining === 0 && computerScore > playerScore) {
+    winner.textContent = `Computer won the game!`;
+  } else if (data.remaining === 0 && playerScore > computerScore) {
+    winner.textContent = `Player won the game!`;
+  }
 };
 
 newDeck.addEventListener('click', getNewDeck);
